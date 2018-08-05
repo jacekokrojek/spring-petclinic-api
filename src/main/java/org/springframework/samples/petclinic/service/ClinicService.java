2a72f7c78288f032e91562f90486a2ee717297ca
javafxdev <javafxdev@gmail.com>
2018-08-05 22:56:27 +0200

    add image to rest api

--- src/main/java/org/springframework/samples/petclinic/service/ClinicService.java
+++ src/main/java/org/springframework/samples/petclinic/service/ClinicService.java
@@ -24,6 +24,7 @@ import org.springframework.samples.petclinic.model.PetType;
 import org.springframework.samples.petclinic.model.Specialty;
 import org.springframework.samples.petclinic.model.Vet;
 import org.springframework.samples.petclinic.model.Visit;
+import org.springframework.web.multipart.MultipartFile;
 
 
 /**
@@ -44,13 +45,13 @@ public interface ClinicService {
 	Collection<Visit> findAllVisits() throws DataAccessException;
 	void saveVisit(Visit visit) throws DataAccessException;
 	void deleteVisit(Visit visit) throws DataAccessException;
-	
+
 	Vet findVetById(int id) throws DataAccessException;
 	Collection<Vet> findVets() throws DataAccessException;
 	Collection<Vet> findAllVets() throws DataAccessException;
 	void saveVet(Vet vet) throws DataAccessException;
 	void deleteVet(Vet vet) throws DataAccessException;
-	
+
 	Owner findOwnerById(int id) throws DataAccessException;
 	Collection<Owner> findAllOwners() throws DataAccessException;
 	void saveOwner(Owner owner) throws DataAccessException;
@@ -62,10 +63,13 @@ public interface ClinicService {
 	Collection<PetType> findPetTypes() throws DataAccessException;
 	void savePetType(PetType petType) throws DataAccessException;
 	void deletePetType(PetType petType) throws DataAccessException;
-	
+
 	Specialty findSpecialtyById(int specialtyId);
 	Collection<Specialty> findAllSpecialties() throws DataAccessException;
 	void saveSpecialty(Specialty specialty) throws DataAccessException;
 	void deleteSpecialty(Specialty specialty) throws DataAccessException;
 
+    String addPetPhoto(int petId, MultipartFile file);
+
+    byte[] loadPetImage(String fileName);
 }
